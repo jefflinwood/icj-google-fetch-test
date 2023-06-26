c41475f181ea55beca19ccea08e53a40f8c70c34
Christian McDonald <christian.mcdonald@utexas.edu>
2023-06-26 18:58:59 -0500

    updating to match icj-project-rig

--- tasks/fetch.js
+++ tasks/fetch.js
@@ -1,51 +1,56 @@
-// native
-const path = require("path");
+import colors from 'ansi-colors';
+import { docToArchieML } from '@newswire/doc-to-archieml';
+import fs from 'fs-extra';
+import { google } from 'googleapis';
+import gulp from 'gulp';
+import path from 'path';
+import { sheetToData } from '@newswire/sheet-to-data';
 
-// packages
-const colors = require("ansi-colors");
-const fs = require("fs-extra");
-
-// internal
-const { google } = require("googleapis");
-const { docToArchieML } = require("@newswire/doc-to-archieml");
-const { sheetToData } = require("@newswire/sheet-to-data");
-const config = require("../project.config.json");
-
-module.exports = (resolve, reject) => {
-  getData();
-  resolve();
-};
+// eslint-disable-next-line no-sync
+const config = fs.readJsonSync('./project.config.json');
 
 async function getData() {
   const auth = await google.auth.getClient({
     scopes: [
-      "https://www.googleapis.com/auth/documents.readonly",
-      "https://www.googleapis.com/auth/spreadsheets.readonly",
-    ],
+      'https://www.googleapis.com/auth/documents.readonly',
+      'https://www.googleapis.com/auth/spreadsheets.readonly'
+    ]
   });
+
   const { files } = config;
   for (const file of files) {
-    const filepath = path.join("data", `${file.name}.json`);
+    const filepath = path.join('src/data', `${file.name}.json`);
 
     let data;
     let color;
 
     switch (file.type) {
-      case "doc":
-        data = await docToArchieML({ documentId: file.fileId, auth });
-        color = "magenta";
+      case 'doc':
+        // eslint-disable-next-line no-await-in-loop
+        data = await docToArchieML({
+          documentId: file.fileId,
+          auth
+        });
+        color = 'magenta';
         break;
-      case "sheet":
-        data = await sheetToData({ spreadsheetId: file.fileId, auth });
-        color = "cyan";
+      case 'sheet':
+        // eslint-disable-next-line no-await-in-loop
+        data = await sheetToData({
+          spreadsheetId: file.fileId,
+          auth
+        });
+        color = 'cyan';
         break;
       default:
         throw new Error(
-          `No data fetching method found for type "${file.type}"`
+          `No data fetching method found for type '${file.type}'`
         );
     }
 
-    await fs.outputJson(filepath, data, { spaces: 2 });
+    // eslint-disable-next-line no-await-in-loop
+    await fs.outputJson(filepath, data, {
+      spaces: 2
+    });
 
     logDownload(file.name, file.fileId, color);
   }
@@ -54,3 +59,12 @@ async function getData() {
 function logDownload(fileName, fileId, color) {
   console.log(colors[color](`Downloaded \`${fileName}\` (${fileId})`));
 }
+
+function fetch(resolve) {
+  getData();
+  resolve();
+}
+
+gulp.task('fetch', fetch);
+
+export default fetch;
